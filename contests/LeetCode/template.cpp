0f5f5093c432263535dbe79fa51ec0d7d1037aa2
Maxim Alexandrov <maxim_0101@bk.ru>
2025-02-16 16:30:31 +0300

    Remove spaces from template

--- contests/LeetCode/template.cpp
+++ contests/LeetCode/template.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-using namespace std;
 
 
 
 int main() {
-    std::ios_base::sync_with_stdio(false); 
-    std::cin.tie(nullptr); 
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     std::cout.tie(nullptr);
     
     return 0;
