f7462177d1bef4389efbf9dc9c706a310b3fb683
Maxim Alexandrov <maxim_0101@bk.ru>
2025-03-02 13:35:58 +0300

    Solve daily problem

---
+++ contests/LeetCode/2570.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& value) {
+    for (const auto& i : value) {
+        out << i << " ";
+    }
+    return out;
+}
+
+class Solution {
+public:
+    std::vector<std::vector<int>> mergeArrays(std::vector<std::vector<int>>& nums1, std::vector<std::vector<int>>& nums2) {
+        std::vector<std::vector<int>> result;
+        size_t n = nums1.size();
+        size_t m = nums2.size();
+        result.reserve(std::max(n, m));
+        size_t i = 0;
+        size_t j = 0;
+        while (i < n && j < m) {
+            if (nums1[i][0] == nums2[j][0]) {
+                result.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                ++i;
+                ++j;
+                continue;
+            }
+            if (nums1[i][0] < nums2[j][0]) {
+                result.push_back(nums1[i]);
+                ++i;
+            } else {
+                result.push_back(nums2[j]);
+                ++j;
+            }
+        }
+        while (i < n) {
+            result.push_back(nums1[i]);
+            ++i;
+        }
+        while (j < m) {
+            result.push_back(nums2[j]);
+            ++j;
+        }
+        return result;
+    }
+};
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    int n = 0;
+    std::cin >> n;
+    std::vector<std::vector<int>> nums1;
+    nums1.reserve(n);
+    for (int i = 0; i < n; ++i) {
+        int id = 0;
+        int val = 0;
+        std::cin >> id >> val;
+        nums1.push_back({id, val});
+    }
+
+    int m = 0;
+    std::cin >> m;
+    std::vector<std::vector<int>> nums2;
+    nums2.reserve(m);
+    for (int i = 0; i < m; ++i) {
+        int id = 0;
+        int val = 0;
+        std::cin >> id >> val;
+        nums2.push_back({id, val});
+    }
+
+    Solution solve;
+    std::cout << solve.mergeArrays(nums1, nums2);
+    return 0;
+}
+
+/**
+
+
+
+**/
